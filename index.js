df2bdf91796a62745eafff58e96e815f0d6c4336
Steven Degutis <sbdegutis@gmail.com>
2017-07-31 15:08:06 -0500

    fixes

--- index.js
+++ index.js
@@ -20,11 +20,16 @@ const party = new Party({
   maxConns: 3
 })
 
+const banned = [
+  '192.227.165.56',
+  '176.31.171.134',
+]
+
 const server = new Server({
   port: config.port,
   origin: config.origin,
   pruneInterval: config.pruneInterval,
-  shouldAllow: (ip) => ip !== '176.31.171.134' && party.canAdd(ip)
+  shouldAllow: (ip) => (banned.indexOf(ip) === -1) && party.canAdd(ip)
 })
 
 
@@ -91,15 +96,29 @@ server.commands = {
       const line = lines[oldIndex]
       line.upvotes = (line.upvotes || 0) + 1
       if (line.upvotes >= config.upvotesNeededToMoveUp) {
-        const newIndex = oldIndex - 1
-        const tmpLine = lines[newIndex]
-        lines[newIndex] = line
-        lines[oldIndex] = tmpLine
-        server.sendToAll({ moved: oldIndex })
+        moveUp(oldIndex)
       }
+    }
+  },
+
+  autotop(ws, bla) {
+    let oldIndex = lines.findIndex((line) => line.uuid === ws.uuid)
+    if (oldIndex < 1) return
+
+    for (let i = oldIndex; i > 0; i--) {
+      moveUp(i)
     }
   }
 
+}
+
+function moveUp(oldIndex) {
+  const newIndex = oldIndex - 1
+  const line = lines[oldIndex]
+  const tmpLine = lines[newIndex]
+  lines[newIndex] = line
+  lines[oldIndex] = tmpLine
+  server.sendToAll({ moved: oldIndex })
 }
 
 function hashForString(str) {
